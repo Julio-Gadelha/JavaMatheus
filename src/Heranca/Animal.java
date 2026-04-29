3dad2c6fd459b9e6eae644dc961ae9780c9b9238
julio gabriel <juliogabriel78956@gmail.com>
2026-04-29 10:30:08 -0300

    Heranca

---
+++ src/Heranca/Animal.java
@@ -0,0 +1,9 @@
+package Heranca;
+
+public class Animal {
+
+    public void QuemSouEu(){
+        System.out.println("Eu sou um animal : ");
+    }
+
+}
---
+++ src/Heranca/test/TestarHeranca.java
@@ -0,0 +1,11 @@
+package Heranca.test;
+
+
+import Heranca.Passaro;
+
+public class TestarHeranca {
+    public static void main(String[] args) {
+        Passaro passaro = new Passaro();
+        passaro.QuemSouEu();
+    }
+}
